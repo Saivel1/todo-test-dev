e0a7ff2c8baba9d4cfc3c66a063a9dae2d39099c
Saivel1 <saivel.mezencev1@gmail.com>
2025-12-18 11:44:44 +0300

    micro fixes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -22,16 +22,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nz!7va#5)exqx!m=wse%+(0reb8iyj)@n)qe96zis!by4i^-mz'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-nz!7va#5)exqx!m=wse%+(0reb8iyj)@n)qe96zis!by4i^-mz")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", True)
 
 ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "").split(",")]
 
@@ -148,7 +146,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# ?
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -167,7 +164,6 @@ REST_FRAMEWORK = {
     'DATETIME_INPUT_FORMATS': ['%Y-%m-%d %H:%M:%S', 'iso-8601'],
 }
 
-# ?
 
 CORS_ALLOWED_ORIGINS = os.getenv(
     'CORS_ALLOWED_ORIGINS',
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('apps.tasks.urls')),
     path('api/auth/', include('apps.users.urls')),
-    path('api-auth/', include('rest_framework.urls')), # ?
+    path('api-auth/', include('rest_framework.urls')), 
 ]
 
 if settings.DEBUG:
